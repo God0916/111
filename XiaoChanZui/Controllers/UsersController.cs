206a34aa832775589ff0e82250bfa986640746fa
1149472629@qq.com <1149472629@qq.com>
2019-02-19 14:14:01 +0800

    布局页

---
+++ XiaoChanZui/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XiaoChanZui.Models;
+
+namespace XiaoChanZui.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        //登录页面
+        public ActionResult Login(string uname,string upwd)
+        {
+            return View();
+        }
+        //注册页面
+        public ActionResult Register(Users u)
+        {
+            return View();
+        }
+    }
+}
