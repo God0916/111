a575ccfca1c7b66b4cc7086ffccb37620ecd75b6
海祥 严 <1729628996@qq.com>
2019-02-19 14:19:32 +0800

    不知道什么的tj

---
+++ XiaoChanZui/Controllers/Demo01Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+
+namespace XiaoChanZui.Controllers
+{
+    public class Demo01Controller : Controller
+    {
+        // GET: Demo01
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
