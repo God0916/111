ef5cd1e0ebf4f9f53d4f5ddff5eec2af0483bc01
张野 <1339274045@qq.com>
2019-02-20 15:09:47 +0800

    11

--- XiaoChanZui/Controllers/AddressesController.cs
+++ XiaoChanZui/Controllers/AddressesController.cs
@@ -19,13 +19,15 @@ namespace XiaoChanZui.Controllers
         {
             return View();
         }
+
+
         [HttpPost]
-        //新增
-        public string Add(Addresses a)
+        public string Add(Addresses addresses, string Sheng, string Shi, string Qu)
         {
-            string ss = JsonConvert.SerializeObject(a);
-            string str = SendHelper.Send("post", "/Addressess/add/", ss);
-            if(str=="1")
+            addresses.Region = Sheng + Shi + Qu;
+            string ss = JsonConvert.SerializeObject(addresses);
+            string str = SendHelper.Send("post", "/api/Addressess/add/", ss);
+            if (str == "1")
             {
                 return "成功";
             }
@@ -34,17 +36,18 @@ namespace XiaoChanZui.Controllers
                 return "失败";
             }
         }
+        
         //显示信息
         public List<Addresses>show()
         {
-            string str = SendHelper.Send("get", "/Addressess/show/");
+            string str = SendHelper.Send("get", "/api/Addressess/show/");
             List<Addresses> list = JsonConvert.DeserializeObject<List<Addresses>>(str);
             return list;
         }
         //删除
         public string Delete(int id)
         {
-            string str = SendHelper.Send("delete", "/Addressess/Del/?id="+id);
+            string str = SendHelper.Send("delete", "/api/Addressess/Del/?id="+id);
             if(str=="1")
             {
                 return "成功";
@@ -65,7 +68,7 @@ namespace XiaoChanZui.Controllers
         public string Edit(Addresses a)
         {
             string ss = JsonConvert.SerializeObject(a);
-            string str = SendHelper.Send("put", "/Addressess/Upd/", ss);
+            string str = SendHelper.Send("put", "/api/Addressess/Upd/", ss);
             if(str=="1")
             {
                 return "成功";
--- XiaoChanZui/Models/Addresses.cs
+++ XiaoChanZui/Models/Addresses.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace XiaoChanZui.Models
 {
     public class Addresses
     {
         public int AddressId { get; set; }
+        [Display(Name ="收货人")]
         public string Consignse { get; set; }
         public int UserId { get; set; }
+        [Display(Name = "收货地址")]
         public string Region { get; set; }
+        [Display(Name = "详细地址")]
         public string DetailAddress { get; set; }
+        [Display(Name = "电话号码")]
         public string Phone { get; set; }
     }
 }
